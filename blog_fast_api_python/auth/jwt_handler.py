17d37ca7f1926f3aa4e0402d354e335c5ac2eacb
TariqMehmood1004 <johnbrrighte7427206@gmail.com>
2025-05-31 22:27:50 +0500

    Convert string to int deltatime for jwt

--- blog_fast_api_python/auth/jwt_handler.py
+++ blog_fast_api_python/auth/jwt_handler.py
@@ -4,7 +4,7 @@ from ..config.config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
 
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
-    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES)))
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
