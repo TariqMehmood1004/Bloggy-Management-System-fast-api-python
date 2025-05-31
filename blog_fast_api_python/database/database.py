f30fc90e806d39e7229ed672dfeea8f9d47890be
TariqMehmood1004 <johnbrrighte7427206@gmail.com>
2025-05-31 23:04:48 +0500

    updated

--- blog_fast_api_python/database/database.py
+++ blog_fast_api_python/database/database.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import os
 
@@ -10,7 +10,14 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_size=10,
+    max_overflow=20,
+    pool_timeout=30,
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
