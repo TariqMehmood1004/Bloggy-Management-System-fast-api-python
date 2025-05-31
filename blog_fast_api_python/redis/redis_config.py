8af8271a352edd325907e80039a0f22b14fff0ed
TariqMehmood1004 <johnbrrighte7427206@gmail.com>
2025-05-31 23:47:00 +0500

    convert string port to integer

--- blog_fast_api_python/redis/redis_config.py
+++ blog_fast_api_python/redis/redis_config.py
@@ -10,7 +10,7 @@ REDIS_HOST = os.getenv("REDIS_HOST")
 REDIS_PORT = os.getenv("REDIS_PORT")
 
 
-redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
+redis_client = redis.Redis(host=REDIS_HOST, port=int(REDIS_PORT), decode_responses=True)
 
 
 async def verify_redis_connection():
