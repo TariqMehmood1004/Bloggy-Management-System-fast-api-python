e439b941963d5699fdad60852ac3750afc010b10
TariqMehmood1004 <johnbrrighte7427206@gmail.com>
2025-05-31 21:46:07 +0500

    Settings up ElastiCache for Redis

--- blog_fast_api_python/redis/redis_config.py
+++ blog_fast_api_python/redis/redis_config.py
@@ -6,7 +6,8 @@ Run Redis:
     For Docker: docker run -d -p 6379:6379 --name redis-server redis
 """
 
-REDIS_HOST = "localhost"
+# REDIS_HOST = "localhost"
+REDIS_HOST = "bloggy-redis-setup-6xyih2.serverless.eun1.cache.amazonaws.com"
 REDIS_PORT = 6379
 
 redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
